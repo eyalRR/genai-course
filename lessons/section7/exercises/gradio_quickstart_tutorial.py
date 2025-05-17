41feec896a970ab8c8d36a0bc232544feb92c66d
Eyal Rabinovich <Eyal.Rabinovich@Maytronics.com>
2025-05-17 21:55:32 +0300

    section 8

---
+++ lessons/section7/exercises/gradio_quickstart_tutorial.py
@@ -0,0 +1,14 @@
+# import gradio as gr
+
+# def greet(name, intensity):
+#     return "Hello, " + name + "!" * int(intensity)
+
+# demo = gr.Interface(
+#     fn=greet,  # The function to wrap
+#     inputs=["text", gr.Slider(minimum=1, maximum=10, step=1)],  # Input components: a textbox and a slider
+#     outputs=["text"],  # Output component: a textbox
+#     title="Simple Greeter",
+#     description="Enter your name and select an intensity for a personalized greeting."
+# )
+
+# demo.launch()
